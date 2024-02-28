c89e99c46051bef1b76278628e345cc00e977ca6
KIRREI <likirrrr@gmail.com>
2024-02-28 22:43:20 +0100

    maj 28 3D obj

--- app.bundle.js
+++ app.bundle.js
@@ -4990,7 +4990,8 @@ headingElements.forEach(function(element) {
                         key: "addLighting",
                         value: function(t) {
                             var e = new ii.Ox3(16777215,5);
-                            e.position.set(61, 25, -5),
+                            e.position.set(-91, 80, 145),
+                                e.intensity = 8; // Modification de l'intensité de la lumière à 10
                                 t.add(e),
                                 t.environment = vi.neutralEnvironment
                         }
@@ -5181,7 +5182,8 @@ headingElements.forEach(function(element) {
                         key: "addLighting",
                         value: function(t) {
                             var e = new ii.Ox3(16777215,5);
-                            e.position.set(61, 25, -5),
+                            e.position.set(191, 225, 245),
+                                e.intensity = 4.5; // Modification de l'intensité de la lumière à 10
                                 t.add(e),
                                 t.environment = vi.neutralEnvironment
                         }
@@ -5245,7 +5247,7 @@ headingElements.forEach(function(element) {
                         var n = this.DOM.main.querySelector(".js-about_ido_coffee");
                         n && (this.obCoffee = new Ti({
                             el: n,
-                            url: "assets/glb/pokeball.gltf",
+                            url: "assets/glb/PokeBall.gltf",
                             scroller: this.state.coffeeScroller,
                             rotation: new ii.USm(this.state.coffeeRotation.x,this.state.coffeeRotation.y,this.state.coffeeRotation.z,"XYZ")
                         }));
